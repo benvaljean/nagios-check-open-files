154592fc29262d9b061681c2a2e00e216025aed7
Benjamin Goodacre <bc@goodacre.org.uk>
2018-03-13 18:50:11 +0700

    First public release

---
+++ check_open_files.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# Nagios Plugin to monitor open files count for a process
+# https://github.com/benvaljean/nagios-check-open-files.git
+#
+# Benjamin Goodacre 2013
+#
+
+version=1.1.25
+
+BASENAME=$(basename $0)
+
+# Exit Status
+okExit=0
+warningExit=1
+criticalExit=2
+unknownExit=3
+
+# Syntax for this script
+function printhelp {
+echo "$BASENAME $version"
+echo "Nagios Plugin to monitor open files count for a process"
+echo "Usage: $BASENAME -p ProgramName -W ProgramWarnlevel -C ProgramCriticlevel"
+echo "Where:
+        -p ProgramName                  : Program name or string listed in ps -ef
+        -W ProgramWarningLevel          : Programs Warning Level (default 400)
+        -C ProgramCriticalLevel         : Programs Critical Level (default 512)"
+exit $unknownExit
+}
+
+# Validate command line parameters
+
+while getopts "hp:W:C:w:c:" optionName; do
+   case "$optionName" in
+        p) ProgName="$OPTARG";;
+        W) ProgWlevel="$OPTARG";;
+        C) ProgClevel="$OPTARG";;
+        *) printhelp;;
+   esac
+done
+
+# Program name is required
+[ -z "$ProgName" ] && printhelp
+
+# Set default values
+[ -z "$ProgWlevel" ] && ProgWlevel=400
+[ -z "$ProgClevel" ] && ProgClevel=512
+
+MESSAGE=""
+
+# Get the PID of program(s)
+PidOfProg="`ps -ef|grep $ProgName|grep -Ewv "grep|$0"|awk '{print $2}'`"
+[ -z "$PidOfProg" ] && echo "UNKNOWN: Not able to determine PID of $ProgName" && exit $unknownExit
+
+NoOfOpenFiles=$(ls -l /proc/$PidOfProg/fd|wc -l)
+if [ $NoOfOpenFiles -ge $ProgClevel ] ;then
+  MESSAGE="pid $ParentPid has $NoOfOpenFiles open files. "
+  CRITICAL=yes
+elif [ $NoOfOpenFiles -ge $ProgWlevel ] ;then
+  MESSAGE="$MESSAGE pid $ParentPid has $NoOfOpenFiles open files. "
+  WARNING=yes
+else
+  MESSAGE="Open FDs = $NoOfOpenFiles PID=$PidOfProg "
+  OK=yes
+fi
+
+# Display Message and exit with correct exit status
+[ "$CRITICAL" = yes ] && echo "FDs CRITICAL: $ProgName $MESSAGE"  && exit $criticalExit
+[ "$WARNING" = yes  ] && echo "FDs WARNING: $ProgName $MESSAGE"  && exit $warningExit
+[ "$OK" = yes  ] && echo "FDs OK: $ProgName $MESSAGE"  && exit $okExit
